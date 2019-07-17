70c6d3ee99ffd9864c9c80bffd02f520b162a6ea
julianbermudez <julianbermudez@eprosima.com>
2019-07-17 13:30:38 +0000

    Refs #6007. Add rmw_node_names.c file.

---
+++ rmw_microxrcedds_c/src/rmw_node_names.c
@@ -0,0 +1,29 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <rmw/rmw.h>
+#include <rmw/error_handling.h>
+
+rmw_ret_t
+rmw_get_node_names(
+  const rmw_node_t * node,
+  rcutils_string_array_t * node_names,
+  rcutils_string_array_t * node_namespaces)
+{
+  (void) node;
+  (void) node_names;
+  (void) node_namespaces;
+  RMW_SET_ERROR_MSG("function not implemeted");
+  return RMW_RET_ERROR;
+}
