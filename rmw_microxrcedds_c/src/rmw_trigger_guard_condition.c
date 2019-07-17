d9c2f56b3aa497abd31af4f489c042625e85800f
julianbermudez <julianbermudez@eprosima.com>
2019-07-17 14:56:52 +0000

    Refs #6007. Add rmw_trigger_guard_condition.c file.

---
+++ rmw_microxrcedds_c/src/rmw_trigger_guard_condition.c
@@ -0,0 +1,25 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <rmw/rmw.h>
+#include <rmw/names_and_types.h>
+#include <rmw/error_handling.h>
+
+rmw_ret_t
+rmw_trigger_guard_condition(const rmw_guard_condition_t * guard_condition)
+{
+  (void) guard_condition;
+  RMW_SET_ERROR_MSG("function not implemeted");
+  return RMW_RET_ERROR;
+}
