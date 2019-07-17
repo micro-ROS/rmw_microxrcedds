1ed107b2a600ac11bb3597988b58034f05a0a362
julianbermudez <julianbermudez@eprosima.com>
2019-07-17 14:24:43 +0000

    Refs #6007. Add rmw_service.c file.

---
+++ rmw_microxrcedds_c/src/rmw_service.c
@@ -0,0 +1,50 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "utils.h"
+
+#include <rmw/rmw.h>
+#include <rmw/allocators.h>
+
+rmw_service_t *
+rmw_create_service(
+  const rmw_node_t * node,
+  const rosidl_service_type_support_t * type_support,
+  const char * service_name,
+  const rmw_qos_profile_t * qos_policies)
+{
+    (void) node;
+    (void) type_support;
+    (void) service_name;
+    (void) qos_policies;
+  EPROS_PRINT_TRACE()
+
+  rmw_service_t * rmw_service = (rmw_service_t *)rmw_allocate(
+    sizeof(rmw_service_t));
+  return rmw_service;
+}
+
+rmw_ret_t
+rmw_destroy_service(
+  rmw_node_t * node,
+  rmw_service_t * service)
+{
+  (void) node;
+
+  EPROS_PRINT_TRACE()
+
+  rmw_free(service);
+
+  return RMW_RET_OK;
+}
