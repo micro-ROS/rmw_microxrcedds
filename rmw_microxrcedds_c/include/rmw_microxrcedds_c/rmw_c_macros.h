ed337663cc7318ffb75b962f3dabab4ceb2cd302
Pablo Garrido <pablogs9@gmail.com>
2019-12-30 10:57:08 +0100

    Microros crazyflie (#21)
    * Added custom transports support
    
    * Remove printf checks
    
    * Custom transport mtu
    
    * Remove printf
    
    * Fixes

--- rmw_microxrcedds_c/include/rmw_microxrcedds_c/rmw_c_macros.h
+++ rmw_microxrcedds_c/include/rmw_microxrcedds_c/rmw_c_macros.h
@@ -15,62 +15,12 @@
 #ifndef RMW_MICROXRCEDDS_C__RMW_C_MACROS_H_
 #define RMW_MICROXRCEDDS_C__RMW_C_MACROS_H_
 
-#if RMW_AVOID_MEMORY_ALLOCATION
 #define RMW_CHECK_TYPE_IDENTIFIERS_MATCH(ElementName, ElementTypeID, ExpectedTypeID, OnFailure) \
   { \
     if (strcmp(ElementTypeID, ExpectedTypeID) != 0) { \
-      char __msg[1024]; \
-      int ret = rcutils_snprintf( \
-        __msg, sizeof(__msg), \
-        #ElementName " implementation '%s'(%p) does not match rmw implementation '%s'(%p)", \
-        ElementTypeID, (const void *)(ElementTypeID), \
-        ExpectedTypeID, (const void *)(ExpectedTypeID)); \
-      if (ret < 0) { \
-        static const char error_msg[] = \
-          "RMW_CHECK_TYPE_IDENTIFIERS_MATCH(): rcutils_snprintf() failed"; \
-        memmove(__msg, error_msg, sizeof(error_msg)); \
-      } \
-      RMW_SET_ERROR_MSG(__msg); \
+      RMW_SET_ERROR_MSG("Implementation identifiers does not match"); \
       OnFailure; \
     } \
   }
-#else  // RMW_AVOID_MEMORY_ALLOCATION
-#define RMW_CHECK_TYPE_IDENTIFIERS_MATCH(ElementName, ElementTypeID, ExpectedTypeID, OnFailure) \
-  { \
-    if (strcmp(ElementTypeID, ExpectedTypeID) != 0) { \
-      int __bytes_that_would_have_been_written = rcutils_snprintf( \
-        NULL, 0, \
-        #ElementName " implementation '%s'(%p) does not match rmw implementation '%s'(%p)", \
-        ElementTypeID, (const void *)(ElementTypeID), \
-        ExpectedTypeID, (const void *)(ExpectedTypeID)); \
-      if (__bytes_that_would_have_been_written < 0) { \
-        RMW_SET_ERROR_MSG( \
-          "RMW_CHECK_TYPE_IDENTIFIERS_MATCH(): rcutils_snprintf(NULL, 0, ...) failed"); \
-        OnFailure; \
-      } else { \
-        char * __msg = \
-          (char *)(rmw_allocate(__bytes_that_would_have_been_written + 1)); \
-        if (NULL == __msg) { \
-          RMW_SET_ERROR_MSG( \
-            "RMW_CHECK_TYPE_IDENTIFIERS_MATCH(): rmw_allocate() failed"); \
-        } else { \
-          int ret = rcutils_snprintf( \
-            __msg, __bytes_that_would_have_been_written + 1, \
-            #ElementName " implementation '%s'(%p) does not match rmw implementation '%s'(%p)", \
-            ElementTypeID, (const void *)(ElementTypeID), \
-            ExpectedTypeID, (const void *)(ExpectedTypeID)); \
-          if (ret < 0) { \
-            RMW_SET_ERROR_MSG( \
-              "RMW_CHECK_TYPE_IDENTIFIERS_MATCH(): rcutils_snprintf() failed"); \
-          } else { \
-            RMW_SET_ERROR_MSG(__msg); \
-          } \
-        } \
-        rmw_free(__msg); \
-        OnFailure; \
-      } \
-    } \
-  }
-#endif
 
 #endif  // RMW_MICROXRCEDDS_C__RMW_C_MACROS_H_
