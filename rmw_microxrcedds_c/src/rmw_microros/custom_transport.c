04766c7866d6d266ab21d9248fa858bae342e9ee
Antonio cuadros <acuadros1995@gmail.com>
2021-06-07 07:55:45 +0200

    Add custom transport weak functions

--- rmw_microxrcedds_c/src/rmw_microros/custom_transport.c
+++ rmw_microxrcedds_c/src/rmw_microros/custom_transport.c
@@ -12,38 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "../types.h"
+
 #include <rmw_microxrcedds_c/config.h>
 #include <rmw/rmw.h>
 #include <rmw/allocators.h>
 #include <rmw/ret_types.h>
 #include <rmw/error_handling.h>
 
-#include "../types.h"
+bool __attribute__((weak)) custom_transport_open(struct uxrCustomTransport * transport) 
+{
+    (void) transport;
+    return false;
+}
 
-rmw_uxrce_transport_params_t rmw_uxrce_transport_default_params;
+bool __attribute__((weak)) custom_transport_close(struct uxrCustomTransport * transport)
+{
+    (void) transport;
+    return false;
+}
+
+size_t __attribute__((weak)) custom_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err)
+{
+    (void) transport;
+    (void) buf;
+    (void) len;
+    (void) err;
+    return 0;
+}
+
+size_t __attribute__((weak)) custom_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err)
+{
+    (void) transport;
+    (void) buf;
+    (void) len;
+    (void) timeout;
+    (void) err;
+    return 0;
+}
+
+rmw_uxrce_transport_params_t rmw_uxrce_transport_default_params =
+{ 
+    .framing = false,
+    .NULL,
+    .open_custom_func = custom_transport_open,
+    .close_custom_func = custom_transport_close,
+    .write_custom_func = custom_transport_write,
+    .read_custom_func = custom_transport_read
+};
 
 rmw_ret_t rmw_uros_set_custom_transport(
-  bool framing,
-  void * args,
-  open_custom_func open_cb,
-  close_custom_func close_cb,
-  write_custom_func write_cb,
-  read_custom_func read_cb)
+        bool framing,
+        void* args,
+        open_custom_func open_cb,
+        close_custom_func close_cb,
+        write_custom_func write_cb,
+        read_custom_func read_cb)
 {
-  if (NULL != open_cb &&
-    NULL != close_cb &&
-    NULL != write_cb &&
-    NULL != read_cb)
-  {
-    rmw_uxrce_transport_default_params.framing = framing;
-    rmw_uxrce_transport_default_params.args = args;
-    rmw_uxrce_transport_default_params.open_cb = open_cb;
-    rmw_uxrce_transport_default_params.close_cb = close_cb;
-    rmw_uxrce_transport_default_params.write_cb = write_cb;
-    rmw_uxrce_transport_default_params.read_cb = read_cb;
-  } else {
-    RMW_SET_ERROR_MSG("Uninitialised arguments.");
-    return RMW_RET_INVALID_ARGUMENT;
-  }
-  return RMW_RET_OK;
+    if (NULL != open_cb &&
+            NULL != close_cb &&
+            NULL != write_cb &&
+            NULL != read_cb)
+    {
+        rmw_uxrce_transport_default_params.framing  = framing;
+        rmw_uxrce_transport_default_params.args     = args;
+        rmw_uxrce_transport_default_params.open_cb  = open_cb;
+        rmw_uxrce_transport_default_params.close_cb = close_cb;
+        rmw_uxrce_transport_default_params.write_cb = write_cb;
+        rmw_uxrce_transport_default_params.read_cb  = read_cb;
+    }
+    else
+    {
+        RMW_SET_ERROR_MSG("Uninitialised arguments.");
+        return(RMW_RET_INVALID_ARGUMENT);
+    }
+    return(RMW_RET_OK);
 }
