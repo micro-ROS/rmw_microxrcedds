3b314d05753b0c3ced39772562e06a56bbecd7b3
Pablo Garrido <pablogs9@gmail.com>
2021-10-20 10:15:35 +0200

    Initial RMW wait modifications

--- rmw_microxrcedds_c/src/rmw_wait.c
+++ rmw_microxrcedds_c/src/rmw_wait.c
@@ -100,7 +100,7 @@ rmw_wait(
   while (item != NULL) {
     rmw_context_impl_t * custom_context = (rmw_context_impl_t *)item->data;
     if (custom_context->need_to_be_ran) {
-      uxr_run_session_until_data(&custom_context->session, per_session_timeout);
+      uxr_run_session_timeout(&custom_context->session, per_session_timeout);
     }
     item = item->next;
   }
