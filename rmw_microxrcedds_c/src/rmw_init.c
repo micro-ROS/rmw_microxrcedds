37c2ee55d38fc0acd4936f6994d528e889a0a5f1
Pablo Garrido <pablogs9@gmail.com>
2022-03-03 14:23:03 +0100

    Fix transport init error (#229)
    * Fix transport init error
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    * Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    (cherry picked from commit 51942f621a03a60f932f788a9c262e119dec3f99)

--- rmw_microxrcedds_c/src/rmw_init.c
+++ rmw_microxrcedds_c/src/rmw_init.c
@@ -274,6 +274,10 @@ rmw_init(
   rmw_ret_t transport_init_ret = rmw_uxrce_transport_init(
     context->impl, options->impl, NULL);
   if (RMW_RET_OK != transport_init_ret) {
+    CLOSE_TRANSPORT(&context_impl->transport);
+    put_memory(&session_memory, &context_impl->mem);
+    context->impl = NULL;
+    RMW_UROS_TRACE_MESSAGE("failed to open transport in rmw_init.")
     return transport_init_ret;
   }
 
