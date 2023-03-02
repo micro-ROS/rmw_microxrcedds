5f2d94c6b29fad31d5114f4f383238583ab0dd50
acuadros95 <acuadros1995@gmail.com>
2023-03-02 12:30:17 +0100

    Add missing RMW method
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

---
+++ rmw_microxrcedds_c/src/rmw_get_gid_for_client.c
@@ -0,0 +1,46 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <rmw/rmw.h>
+#include <rmw_microxrcedds_c/rmw_c_macros.h>
+
+#include "./rmw_microros_internal/types.h"
+#include "./rmw_microros_internal/error_handling_internal.h"
+
+rmw_ret_t
+rmw_get_gid_for_client(
+  const rmw_client_t * client,
+  rmw_gid_t * gid)
+{
+  RMW_CHECK_ARGUMENT_FOR_NULL(client, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_ARGUMENT_FOR_NULL(gid, RMW_RET_INVALID_ARGUMENT);
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    client->implementation_identifier,
+    RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+
+  rmw_uxrce_client_t * custom_client = (rmw_uxrce_client_t *)client->data;
+
+  if (sizeof(uxrObjectId) > RMW_GID_STORAGE_SIZE) {
+    RMW_UROS_TRACE_MESSAGE("Not enough memory for impl ids")
+    return RMW_RET_ERROR;
+  }
+
+  memset(gid->data, 0, RMW_GID_STORAGE_SIZE);
+  memcpy(
+    gid->data,
+    &custom_client->client_id,
+    sizeof(uxrObjectId));
+
+  return RMW_RET_OK;
+}
